9fe635f9936600a51698d27dbbe3b7a2b0d50367
soraa-ru <chelseaelishaantalanlucena1904@gmail.com>
2026-05-19 03:41:32 +0800

    Newmain.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -14,4 +18,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
